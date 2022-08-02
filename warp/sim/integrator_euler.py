7d4d686617e2b8122d3e51019f4b7c487a864bb4
Eric Heiden <eric-heiden@outlook.com>
2022-08-02 15:00:11 -0700

    Fix Euler rigid contact handling

--- warp/sim/integrator_euler.py
+++ warp/sim/integrator_euler.py
@@ -991,9 +991,9 @@ def eval_rigid_contacts(
 
     # body position in world space
     margin = contact_margin[tid]
+    n = contact_normal[tid]
     bx_a = -margin * n  
     bx_b = wp.vec3(0.0)
-    n = contact_normal[tid]
     if (body_a >= 0):
         X_wb_a = body_q[body_a]
         X_com_a = body_com[body_a]
