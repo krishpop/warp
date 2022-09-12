641215aa4505c8df7950376d21a15de2813c76f6
Eric Heiden <eric-heiden@outlook.com>
2022-09-12 14:05:45 -0700

    Humanoid demo with PBD

--- examples/example_sim_humanoid.py
+++ examples/example_sim_humanoid.py
@@ -28,12 +28,12 @@ wp.init()
 
 class Robot:
 
-    frame_dt = 1.0/60.0
+    frame_dt = 1.0/ (60.0)
 
-    episode_duration = 5.0      # seconds
+    episode_duration = 2.6  # 5.0      # seconds
     episode_frames = int(episode_duration/frame_dt)
 
-    sim_substeps = 16
+    sim_substeps = 10
     sim_dt = frame_dt / sim_substeps
     sim_steps = int(episode_duration / sim_dt)
    
@@ -49,7 +49,14 @@ class Robot:
 
         self.num_envs = num_envs
 
-        wp.sim.parse_mjcf(os.path.join(os.path.dirname(__file__), "assets/nv_humanoid.xml"), articulation_builder,
+        self.points_a = []
+        self.points_b = []
+        # number of contact points to visualize
+        self.max_contact_count = 0
+
+        wp.sim.parse_mjcf(
+            os.path.join(os.path.dirname(__file__), "assets/nv_humanoid.xml"),
+            articulation_builder,
             stiffness=0.0,
             damping=0.1,
             armature=0.007,
@@ -58,11 +65,15 @@ class Robot:
             contact_kd=1.e+2,
             contact_kf=1.e+2,
             contact_mu=0.5,
+            contact_restitution=0.8,
             limit_ke=1.e+2,
             limit_kd=1.e+1)
 
         for i in range(num_envs):
             builder.add_rigid_articulation(articulation_builder)
+            if i == 0:
+                self.dof_q = len(builder.joint_q)
+                self.dof_qd = len(builder.joint_qd)
  
             coord_count = 28 
             dof_count = 27
@@ -71,9 +82,8 @@ class Robot:
             dof_start = i*dof_count
 
             # position above ground and rotate to +y up
-            builder.joint_q[coord_start:coord_start+3] = [i*2.0, 1.70, 0.0]
+            builder.joint_q[coord_start:coord_start+3] = [i*2.0 + 2.3, 1.70, 1.2]
             builder.joint_q[coord_start+3:coord_start+7] = wp.quat_from_axis_angle((1.0, 0.0, 0.0), -math.pi*0.5)
-
 
         # finalize model
         self.model = builder.finalize(device)
@@ -81,8 +91,16 @@ class Robot:
         self.model.joint_attach_ke *= 8.0
         self.model.joint_attach_kd *= 2.0
 
+        print("joint_target: ", self.model.joint_target.numpy())
+        print("joint_target_ke: ", self.model.joint_target_ke.numpy())
 
-        self.integrator = wp.sim.SemiImplicitIntegrator()
+        # self.integrator = wp.sim.SemiImplicitIntegrator()
+        self.integrator = wp.sim.XPBDIntegrator(
+            iterations=2,
+            joint_positional_relaxation=0.8,
+            joint_angular_relaxation=0.5)
+        self.integrator.contact_con_weighting = False
+        self.integrator.contact_normal_relaxation = 0.5
 
         #-----------------------
         # set up Usd renderer
@@ -111,15 +129,31 @@ class Robot:
         profiler = {}
 
         # create update graph
-        wp.capture_begin()
+        # wp.capture_begin()
 
-        # simulate
-        for i in range(0, self.sim_substeps):
-            self.state.clear_forces()
-            self.state = self.integrator.simulate(self.model, self.state, self.state, self.sim_dt)
-            self.sim_time += self.sim_dt
+        # # simulate
+        # for i in range(0, self.sim_substeps):
+        #     self.state.clear_forces()
+        #     wp.sim.collide(self.model, self.state)
+        #     self.state = self.integrator.simulate(self.model, self.state, self.state, self.sim_dt)
+        #     self.sim_time += self.sim_dt
                 
-        graph = wp.capture_end()
+        # graph = wp.capture_end()
+
+        
+    
+        q_history = []
+        q_history.append(self.state.body_q.numpy().copy())
+        qd_history = []
+        qd_history.append(self.state.body_qd.numpy().copy())
+        delta_history = []
+        delta_history.append(self.state.body_deltas.numpy().copy())
+        num_con_history = []
+        num_con_history.append(self.model.contact_inv_weight.numpy().copy())
+
+        joint_q_history = []
+        joint_q = wp.zeros_like(self.model.joint_q)
+        joint_qd = wp.zeros_like(self.model.joint_qd)
 
 
         # simulate 
@@ -138,15 +172,29 @@ class Robot:
 
                 self.renderer.save()
 
-
-            for f in range(0, self.episode_frames):
+            from tqdm import trange
+            for f in trange(0, self.episode_frames):
                 
                 for i in range(0, self.sim_substeps):
                     self.state.clear_forces()
                     
-                    random_actions = False
+                    # keep floating base fixed
+                    # wp.sim.eval_ik(self.model, self.state, joint_q, joint_qd)
+                    # q = joint_q.numpy().copy()
+                    # q[:3] = [2.3, 1.70, 1.2]
+                    # q[3:7] = wp.quat_from_axis_angle((1.0, 0.0, 0.0), -math.pi*0.5)
+                    # qd = joint_qd.numpy().copy()
+                    # qd[:6] = 0.0
+                    # wp.sim.eval_fk(
+                    #     self.model,
+                    #     wp.array(q, dtype=wp.float32, device=joint_q.device),
+                    #     wp.array(qd, dtype=wp.float32, device=joint_q.device),
+                    #     None,
+                    #     self.state)
                     
+                    random_actions = False
                     if (random_actions):
+                        act = np.zeros(len(self.model.joint_qd))
                         scale = np.array([200.0,
                                         200.0,
                                         200.0,
@@ -168,13 +216,54 @@ class Robot:
                                         100.0,
                                         100.0,
                                         200.0])
+                        for j in range(self.num_envs):
+                            act[j*self.dof_qd+6:(j+1)*self.dof_qd] = np.clip((np.random.rand(self.dof_qd-6)*2.0 - 1.0)*1000.0, a_min=-1.0, a_max=1.0)*scale*0.35
 
-                        act = np.zeros(len(self.model.joint_qd))
-                        act[6:] = np.clip((np.random.rand(len(self.model.joint_qd)-6)*2.0 - 1.0)*1000.0, a_min=-1.0, a_max=1.0)*scale*0.35
+                        # act[6:] = np.clip((np.random.rand(len(self.model.joint_qd)-6)*2.0 - 1.0)*1000.0, a_min=-1.0, a_max=1.0)*scale*0.35
                         self.model.joint_act.assign(act)
 
+                    wp.sim.collide(self.model, self.state)
                     self.state = self.integrator.simulate(self.model, self.state, self.state, self.sim_dt)
                     self.sim_time += self.sim_dt
+
+                    # rigid_contact_count = self.model.rigid_contact_count.numpy()[0]
+                    # self.max_contact_count = max(self.max_contact_count, rigid_contact_count)                    
+                    # if i == 0 and rigid_contact_count > 0:
+                    #     qs = self.state.body_q.numpy()
+
+                    #     contact_points_a = self.model.rigid_contact_point0.numpy()
+                    #     body_a = self.model.rigid_contact_body0.numpy()
+                    #     self.points_a[:rigid_contact_count] = [wp.transform_point(qs[body], wp.vec3(*contact_points_a[i])) for i, body in enumerate(body_a)]
+
+                    #     contact_points_b = self.model.rigid_contact_point1.numpy()
+                    #     body_b = self.model.rigid_contact_body1.numpy()
+                    #     self.points_b[:rigid_contact_count] = [wp.transform_point(qs[body], wp.vec3(*contact_points_b[i])) for i, body in enumerate(body_b)]
+
+                    
+                    q_history.append(self.state.body_q.numpy().copy())
+                    qd_history.append(self.state.body_qd.numpy().copy())
+                    delta_history.append(self.state.body_deltas.numpy().copy())
+                    num_con_history.append(self.model.contact_inv_weight.numpy().copy())
+
+                    wp.sim.eval_ik(self.model, self.state, joint_q, joint_qd)
+                    joint_q_history.append(joint_q.numpy().copy())
+
+
+
+                    if self.max_contact_count > 0:
+                        rigid_contact_count = self.model.rigid_contact_count.numpy()[0]
+                        self.max_contact_count = max(self.max_contact_count, rigid_contact_count)
+                        
+                        if rigid_contact_count > 0:
+                            qs = self.state.body_q.numpy()
+
+                            contact_points_a = self.model.rigid_contact_point0.numpy()
+                            body_a = self.model.rigid_contact_body0.numpy()
+                            self.points_a[:rigid_contact_count] = [(wp.transform_point(qs[body], wp.vec3(*contact_points_a[i])) if body >= 0 else wp.vec3(*contact_points_a[i])) for i, body in enumerate(body_a)]
+
+                            contact_points_b = self.model.rigid_contact_point1.numpy()
+                            body_b = self.model.rigid_contact_body1.numpy()
+                            self.points_b[:rigid_contact_count] = [(wp.transform_point(qs[body], wp.vec3(*contact_points_b[i])) if body >= 0 else wp.vec3(*contact_points_b[i])) for i, body in enumerate(body_b)]
 
 
                 if (self.render):
@@ -186,6 +275,11 @@ class Robot:
                             
                             self.renderer.begin_frame(self.render_time)
                             self.renderer.render(self.state)
+
+                            if self.max_contact_count > 0:
+                                self.renderer.render_points("contact_points_a", np.array(self.points_a), radius=0.05)
+                                self.renderer.render_points("contact_points_b", np.array(self.points_b), radius=0.05)
+
                             self.renderer.end_frame()
 
                     self.renderer.save()
@@ -197,6 +291,109 @@ class Robot:
         avg_steps_second = 1000.0*float(self.num_envs)/avg_time
 
         print(f"envs: {self.num_envs} steps/second {avg_steps_second} avg_time {avg_time}")
+
+        
+        if False:
+            import matplotlib.pyplot as plt
+            q_history = np.array(q_history)
+            qd_history = np.array(qd_history)
+            delta_history = np.array(delta_history)
+            num_con_history = np.array(num_con_history)
+            # print("max num_con_history:", np.max(num_con_history))
+
+            body_indices = [9]
+
+            fig, ax = plt.subplots(len(body_indices), 7, figsize=(10, 10), squeeze=False)
+            fig.subplots_adjust(hspace=0.2, wspace=0.2)
+            for i, j in enumerate(body_indices):
+                ax[i,0].set_title(f"Body {j} Position")
+                ax[i,0].grid()
+                ax[i,1].set_title(f"Body {j} Orientation")
+                ax[i,1].grid()
+                ax[i,2].set_title(f"Body {j} Linear Velocity")
+                ax[i,2].grid()
+                ax[i,3].set_title(f"Body {j} Angular Velocity")
+                ax[i,3].grid()
+                ax[i,4].set_title(f"Body {j} Linear Delta")
+                ax[i,4].grid()
+                ax[i,5].set_title(f"Body {j} Angular Delta")
+                ax[i,5].grid()
+                ax[i,6].set_title(f"Body {j} Num Contacts")
+                ax[i,6].grid()
+                ax[i,0].plot(q_history[:,j,:3])        
+                ax[i,1].plot(q_history[:,j,3:])
+                ax[i,2].plot(qd_history[:,j,3:])
+                ax[i,3].plot(qd_history[:,j,:3])
+                ax[i,4].plot(delta_history[:,j,3:])
+                ax[i,5].plot(delta_history[:,j,:3])
+                ax[i,6].plot(num_con_history[:,j])
+                ax[i,0].set_xlim(0, self.sim_steps)
+                ax[i,1].set_xlim(0, self.sim_steps)
+                ax[i,2].set_xlim(0, self.sim_steps)
+                ax[i,3].set_xlim(0, self.sim_steps)
+                ax[i,4].set_xlim(0, self.sim_steps)
+                ax[i,5].set_xlim(0, self.sim_steps)
+                ax[i,6].set_xlim(0, self.sim_steps)
+                ax[i,6].yaxis.get_major_locator().set_params(integer=True)
+            plt.show()
+
+        if True:
+            import matplotlib.pyplot as plt
+            joint_q_history = np.array(joint_q_history)
+            dof_q = joint_q_history.shape[1]
+            ncols = int(np.ceil(np.sqrt(dof_q)))
+            nrows = int(np.ceil(dof_q / float(ncols)))
+            fig, axes = plt.subplots(
+                ncols=ncols,
+                nrows=nrows,
+                constrained_layout=True,
+                figsize=(ncols * 3.5, nrows * 3.5),
+                squeeze=False,
+                sharex=True
+            )
+
+            joint_id = 0
+            joint_names = {
+                wp.sim.JOINT_BALL.val : "ball", 
+                wp.sim.JOINT_REVOLUTE.val : "hinge", 
+                wp.sim.JOINT_PRISMATIC.val : "slide", 
+                wp.sim.JOINT_UNIVERSAL.val : "universal",
+                wp.sim.JOINT_COMPOUND.val : "compound",
+                wp.sim.JOINT_FREE.val : "free", 
+                wp.sim.JOINT_FIXED.val : "fixed"
+            }
+            joint_lower = self.model.joint_limit_lower.numpy()
+            joint_upper = self.model.joint_limit_upper.numpy()
+            joint_type = self.model.joint_type.numpy()
+            while joint_id < len(joint_type)-1 and joint_type[joint_id] == wp.sim.JOINT_FIXED.val:
+                # skip fixed joints
+                joint_id += 1
+            q_start = self.model.joint_q_start.numpy()
+            qd_start = self.model.joint_qd_start.numpy()
+            qd_i = qd_start[joint_id]
+            for dim in range(ncols * nrows):
+                ax = axes[dim // ncols, dim % ncols]
+                if dim >= dof_q:
+                    ax.axis("off")
+                    continue
+                ax.grid()
+                ax.plot(joint_q_history[:, dim])
+                if joint_type[joint_id] != wp.sim.JOINT_FREE.val:
+                    lower = joint_lower[qd_i]
+                    if abs(lower) < 2*np.pi:
+                        ax.axhline(lower, color="red")
+                    upper = joint_upper[qd_i]
+                    if abs(upper) < 2*np.pi:
+                        ax.axhline(upper, color="red")
+                joint_name = joint_names[joint_type[joint_id]]
+                ax.set_title(f"$\\mathbf{{q_{{{dim}}}}}$ ({self.model.joint_name[joint_id]} / {joint_name} {joint_id})")
+                if joint_id < self.model.joint_count-1 and q_start[joint_id+1] == dim+1:
+                    joint_id += 1
+                    qd_i = qd_start[joint_id]
+                else:
+                    qd_i += 1
+            plt.tight_layout()
+            plt.show()
 
         return 1000.0*float(self.num_envs)/avg_time
 
@@ -230,10 +427,10 @@ if profile:
     plt.xscale('log')
     plt.xlabel("Number of Envs")
     plt.yscale('log')
-    plt.ylabel("Steps/Second")
+    plt.ylabel("Steps/Second") 
     plt.show()
 
 else:
 
-    robot = Robot(render=True, num_envs=1)
+    robot = Robot(render=True, num_envs=1, device="cpu") #wp.get_preferred_device())
     robot.run()
