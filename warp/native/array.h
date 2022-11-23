40bab7c967716c6982fd2b7200516dc4fb96b71c
Eric Heiden <eheiden@nvidia.com>
2022-11-22 22:02:10 -0800

    Make empty array constructors CUDA callable

--- warp/native/array.h
+++ warp/native/array.h
@@ -126,8 +126,8 @@ inline CUDA_CALLABLE void adj_print(shape_t s, shape_t& shape_t) {}
 template <typename T>
 struct array_t
 {
-    array_t() {}    
-    array_t(int) {} // for backward a = 0 initialization syntax
+    inline CUDA_CALLABLE array_t() {}    
+    inline CUDA_CALLABLE array_t(int) {} // for backward a = 0 initialization syntax
 
     array_t(T* data, int size) : data(data) {
         // constructor for 1d array
