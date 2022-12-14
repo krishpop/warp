d601311ed5ba63cb053247a1190df77c051eab46
Krishnan <krshna@stanford.edu>
2022-12-14 18:22:06 -0500

    removed unnecessary prints

--- warp/sim/import_mjcf.py
+++ warp/sim/import_mjcf.py
@@ -61,6 +61,7 @@ def parse_mjcf(
 
     def parse_mesh(geom):
         import trimesh
+
         faces = []
         vertices = []
         stl_file = next(
@@ -94,21 +95,7 @@ def parse_mjcf(
                 body_ori_euler[np.nonzero(body_ori_euler)[0].item()] / 180 * np.pi
             )
             body_ori = wp.utils.quat_from_axis_angle(body_axis, body_angle)
-            for val in [
-                "body_ori_euler",
-                body_ori_euler,
-                "body_angle",
-                body_angle,
-                "body_axis",
-                body_axis,
-                "body_ori",
-                body_ori,
-                "body_pos",
-                body_pos,
-            ]:
-                print(val)
         else:
-            print("body_ori_euler", body_ori_euler)
             body_ori = wp.quat_identity()
 
         # -----------------
