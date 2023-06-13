10b06651255336bc1b18e6067b667c811ee1e441
Eric Heiden <eric-heiden@outlook.com>
2023-06-12 22:39:22 -0700

    WIP MANO hand mesh skinning

--- examples/example_sim_mano_hand.py
+++ examples/example_sim_mano_hand.py
@@ -21,6 +21,8 @@ import warp as wp
 import warp.sim
 import warp.sim.render
 
+import skinning
+
 wp.init()
 
 
@@ -40,10 +42,40 @@ def animate_joints(
     wp.atomic_add(time, 0, dt)
 
 
+@wp.kernel
+def skin_mesh(
+    body_q: wp.array(dtype=wp.transform),
+    orig_inv_body_q: wp.array(dtype=wp.transform),
+    vertices: wp.array(dtype=wp.vec3),
+    orig_vertices: wp.array(dtype=wp.vec3),
+    max_bones_per_vertex: int,
+    skinning_ids: wp.array(dtype=int),
+    skinning_weights: wp.array(dtype=float)
+):
+    vert_nr = wp.tid()
+
+    v_orig = orig_vertices[vert_nr]
+    v_sum = wp.vec3(0.0, 0.0, 0.0)
+    w_sum = float(0.0)
+    first = vert_nr * max_bones_per_vertex
+
+    for i in range(max_bones_per_vertex):
+        id = skinning_ids[first + i]
+        w = skinning_weights[first + i]
+        # TODO transform vector or point?
+        # Matthias:  v_sum = v_sum + body_q[id] * orig_inv_body_q[id] * v_orig * w
+        tf = body_q[id] * orig_inv_body_q[id]
+        v_sum = v_sum + wp.transform_point(tf, v_orig) * w
+        w_sum = w_sum + w
+
+    if w_sum > 0.0:
+        vertices[vert_nr] = v_sum / w_sum
+
+
 class Example:
     frame_dt = 1.0 / 60.0
 
-    episode_duration = 20.0  # seconds
+    episode_duration = 120.0  # seconds
     episode_frames = int(episode_duration / frame_dt)
 
     sim_substeps = 10
@@ -53,16 +85,16 @@ class Example:
     sim_time = 0.0
 
     def __init__(self, stage=None, render=True):
-        builder = wp.sim.ModelBuilder()
 
         self.enable_rendering = render
+        self.device = wp.get_device("cuda")
 
         builder = wp.sim.ModelBuilder()
 
         wp.sim.parse_mjcf(
             os.path.join(os.path.dirname(__file__), "assets/mano_hand/auto_mano_fixed_base.xml"),
             builder,
-            xform=wp.transform((0.0, 0.0, 0.0), wp.quat_rpy(-np.pi / 2, np.pi * 0.75, np.pi / 2)),
+            xform=wp.transform((0.1, 0.0, 0.0), wp.quat_rpy(np.pi / 2, 0.0, 0.0)),
             density=1e3,
             armature=0.1,
             stiffness=10.0,
@@ -85,6 +117,57 @@ class Example:
             builder.joint_target_ke[i] = 5000.0
             builder.joint_target_kd[i] = 1.0
 
+        # skinning
+
+        self.max_bones_per_vertex = 4
+        _, _, vertices, tri_ids = skinning.read_obj(os.path.join(
+            os.path.dirname(__file__), "assets/mano_hand/mano_watertight.obj"))
+
+        self.num_verts = len(vertices)
+        self.vertices = wp.array(vertices, dtype=wp.vec3, device=self.device)
+        self.orig_verts = wp.array(vertices, dtype=wp.vec3, device=self.device)
+        self.tri_ids = wp.array(tri_ids, dtype=wp.vec3, device=self.device)
+
+        self.skinned_mesh = wp.sim.Mesh(vertices, tri_ids)
+
+        # builder.add_shape_mesh(
+        #     body=-1,
+        #     mesh=self.skinned_mesh,
+        #     has_ground_collision=False,
+
+        # )
+
+        num_bones = len(builder.body_q)
+        bone_transforms = []
+        bone_shapes = []
+
+        self.orig_inv_body_q = []
+
+        for i in range(num_bones):
+            h = 0.1
+            for shape in builder.body_shapes[i]:
+                h = 2.0 * builder.shape_geo_scale[shape][1]
+            bone_shapes.append(wp.vec3(h, 0.0, 0.0))
+            self.orig_inv_body_q.append(wp.transform_inverse(builder.body_q[i]))
+
+            # TODO does this mean transform point or vector?
+            # Matthias:  pos = wp.transform(builder.body_q[i], wp.vec3(-0.5 * h, 0.0, 0.0))
+            pos = wp.transform_vector(builder.body_q[i], wp.vec3(-0.5 * h, 0.0, 0.0))
+            mat = wp.quat_to_matrix(wp.transform_get_rotation(builder.body_q[i]))
+            bone_transforms.append([
+                [mat[0][0], mat[0][1], mat[0][2], pos[0]],
+                [mat[1][0], mat[1][1], mat[1][2], pos[1]],
+                [mat[2][0], mat[2][1], mat[2][2], pos[2]],
+                [0.0, 0.0, 0.0, 1.0]])
+
+        self.orig_inv_body_q = wp.array(self.orig_inv_body_q, dtype=wp.transform, device=self.device)
+
+        skinning_ids, skinning_weights = skinning.compute_skinning_info(
+            bone_transforms, bone_shapes, vertices, tri_ids, self.max_bones_per_vertex, resolution=30, device=self.device)
+
+        self.skinning_ids = skinning_ids
+        self.skinning_weights = skinning_weights
+
         # finalize model
         self.model = builder.finalize()
         self.model.ground = False
@@ -95,8 +178,11 @@ class Example:
 
         self.renderer = None
         if render:
-            # self.renderer = wp.sim.render.SimRendererOpenGL(self.model, stage, show_joints=True, scaling=40.0, draw_axis=False)
-            self.renderer = wp.sim.render.SimRendererUsd(self.model, stage, scaling=500.0)
+            self.renderer = wp.sim.render.SimRendererOpenGL(
+                self.model, stage, show_joints=True, scaling=40.0, draw_axis=False)
+            # self.renderer = wp.sim.render.SimRendererUsd(self.model, stage, scaling=500.0)
+
+            self.renderer.render_mesh("hand_mesh", self.vertices.numpy(), self.tri_ids.numpy())
 
     def update(self):
         for _ in range(self.sim_substeps):
@@ -111,16 +197,27 @@ class Example:
                     self.model.joint_limit_lower,
                     self.model.joint_limit_upper,
                     self.model.joint_target,
-                    self.time_array]
-            )
+                    self.time_array],
+                device=self.device)
             self.integrator.simulate(self.model, self.state_0, self.state_1, self.sim_dt)
             self.state_0, self.state_1 = self.state_1, self.state_0
+
+        wp.launch(
+            skin_mesh,
+            dim=self.num_verts,
+            inputs=[
+                self.state_0.body_q, self.orig_inv_body_q,
+                self.vertices, self.orig_verts,
+                self.max_bones_per_vertex,
+                self.skinning_ids, self.skinning_weights],
+            device=self.device)
 
     def render(self, is_live=False):
         time = 0.0 if is_live else self.sim_time
 
         self.renderer.begin_frame(time)
         self.renderer.render(self.state_0)
+        self.renderer.render_mesh("hand_mesh", self.vertices.numpy(), self.tri_ids.numpy())
         self.renderer.end_frame()
 
     def run(self):
