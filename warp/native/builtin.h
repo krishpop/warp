f1fb3067f9f043f3cbeaeddb0c38432aec0d58be
Eric Heiden <eheiden@nvidia.com>
2023-07-10 10:12:34 +0200

    Update Warp

--- warp/native/builtin.h
+++ warp/native/builtin.h
@@ -345,9 +345,9 @@ inline CUDA_CALLABLE T clamp(T x, T a, T b) { return min(max(a, x), b); } \
 inline CUDA_CALLABLE T floordiv(T a, T b) { return a/b; } \
 inline CUDA_CALLABLE T nonzero(T x) { return x == T(0) ? T(0) : T(1); } \
 inline CUDA_CALLABLE T sqrt(T x) { return 0; } \
-inline CUDA_CALLABLE T logical_and(T a, T b) { return a&b; } \
-inline CUDA_CALLABLE T logical_or(T a, T b) { return a|b; } \
-inline CUDA_CALLABLE T logical_xor(T a, T b) { return a^b; } \
+inline CUDA_CALLABLE T bit_and(T a, T b) { return a&b; } \
+inline CUDA_CALLABLE T bit_or(T a, T b) { return a|b; } \
+inline CUDA_CALLABLE T bit_xor(T a, T b) { return a^b; } \
 inline CUDA_CALLABLE T lshift(T a, T b) { return a<<b; } \
 inline CUDA_CALLABLE T rshift(T a, T b) { return a>>b; } \
 inline CUDA_CALLABLE T invert(T x) { return ~x; } \
@@ -366,9 +366,9 @@ inline CUDA_CALLABLE void adj_floordiv(T a, T b, T& adj_a, T& adj_b, T adj_ret) 
 inline CUDA_CALLABLE void adj_step(T x, T& adj_x, T adj_ret) { } \
 inline CUDA_CALLABLE void adj_nonzero(T x, T& adj_x, T adj_ret) { } \
 inline CUDA_CALLABLE void adj_sqrt(T x, T adj_x, T& adj_ret) { } \
-inline CUDA_CALLABLE void adj_logical_and(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
-inline CUDA_CALLABLE void adj_logical_or(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
-inline CUDA_CALLABLE void adj_logical_xor(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
+inline CUDA_CALLABLE void adj_bit_and(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
+inline CUDA_CALLABLE void adj_bit_or(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
+inline CUDA_CALLABLE void adj_bit_xor(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
 inline CUDA_CALLABLE void adj_lshift(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
 inline CUDA_CALLABLE void adj_rshift(T a, T b, T& adj_a, T& adj_b, T adj_ret) { } \
 inline CUDA_CALLABLE void adj_invert(T x, T adj_x, T& adj_ret) { }
@@ -1059,8 +1059,9 @@ CUDA_CALLABLE inline void copy(T& dest, const T& src)
 template <typename T>
 CUDA_CALLABLE inline void adj_copy(T& dest, const T& src, T& adj_dest, T& adj_src)
 {
-    adj_src += adj_dest;
-    adj_dest = T(0.0);
+    // nop, this is non-differentiable operation since it violates SSA
+    adj_src = adj_dest;
+    adj_dest = T(0);
 }
 
 
