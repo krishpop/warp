Update grad_utils, fix gradients for array.assign

warp/tape.py
@@ -63,13 +63,15 @@ class Tape:
     def forward(self, check_nans=True):
 
         # run launches forwards
-        for launch in self.launches:
+        from tqdm import tqdm
+        for launch in tqdm(self.launches, desc="Tape forward pass"):
 
             kernel = launch[0]
             dim = launch[1]
-            inputs = launch[2]
-            outputs = launch[3]
-            device = launch[4]
+            max_blocks = launch[2]
+            inputs = launch[3]
+            outputs = launch[4]
+            device = launch[5]
 
             wp.launch(
                 kernel=kernel,
@@ -83,10 +85,11 @@ class Tape:
                     if isinstance(o, wp.array):
                         if np.isnan(o.numpy()).any():
                             raise RuntimeError("Warp: Error, NaN detected in output array. Check your kernel for errors.")
-                for i in inputs:
-                    if isinstance(i, wp.array):
-                        if np.isnan(i.numpy()).any():
-                            raise RuntimeError("Warp: Error, NaN detected in input array. Check your kernel for errors.")
+                # TODO handle array of structs
+                # for i in inputs:
+                #     if isinstance(i, wp.array):
+                #         if np.isnan(i.numpy()).any():
+                #             raise RuntimeError("Warp: Error, NaN detected in input array. Check your kernel for errors.")
 
     # adj_outputs is a mapping from output tensor -> adjoint of the output
     # after running backward the gradients of tensors may be retrieved by:
@@ -124,7 +127,11 @@ class Tape:
         # existing code before we added wp.array.grad attribute
         if grads:
             for a, g in grads.items():
-                a.grad = g
+                if a.grad is None:
+                    a.grad = g
+                else:
+                    # ensure we can capture this backward pass in a CUDA graph
+                    a.grad.assign(g)
                 self.const_gradients.add(a)
 
         # run launches backwards
@@ -164,8 +171,8 @@ class Tape:
                 )
 
     # record a kernel launch on the tape
-    def record_launch(self, kernel, dim, max_blocks, inputs, outputs, device):
-        self.launches.append([kernel, dim, max_blocks, inputs, outputs, device])
+    def record_launch(self, kernel, dim, max_blocks, inputs, outputs, device, meta_data=None):
+        self.launches.append([kernel, dim, max_blocks, inputs, outputs, device, meta_data])
 
     def record_func(self, backward, arrays):
         """