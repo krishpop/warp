e7c34bdc577d95cf07ac774666327c1aac5af070
Eric Heiden <eric-heiden@outlook.com>
2023-11-02 15:50:03 -0700

    Add scaling to Allegro hand env

--- examples/env/env_allegro.py
+++ examples/env/env_allegro.py
@@ -25,23 +25,22 @@ from environment import Environment, run_env
 
 class AllegroEnvironment(Environment):
     sim_name = "example_sim_allegro"
-    env_offset = (0.5, 0.0, 0.5)
-    opengl_render_settings = dict(scaling=4.0)
-    usd_render_settings = dict(scaling=200.0)
     episode_duration = 8.0
 
     sim_substeps_euler = 64
-    sim_substeps_xpbd = 8
+    sim_substeps_xpbd = 5
 
     rigid_contact_margin = 0.001
     rigid_mesh_contact_max = 100
 
     num_envs = 100
 
+    scale = 1.0
+
     show_joints = False
 
     xpbd_settings = dict(
-        iterations=10,
+        iterations=2,
         joint_linear_relaxation=1.0,
         joint_angular_relaxation=0.45,
         rigid_contact_relaxation=1.0,
@@ -53,6 +52,22 @@ class AllegroEnvironment(Environment):
     # edge_sdf_iter = 20
 
     use_graph_capture = True
+    env_offset = (0.5 * scale, 0.0, 0.5 * scale)
+    opengl_render_settings = dict(scaling=4.0/scale)
+    usd_render_settings = dict(scaling=200.0/scale)
+
+    def load_mesh(self, filename, use_meshio=True):
+        if use_meshio:
+            import meshio
+            m = meshio.read(filename)
+            mesh_points = np.array(m.points)
+            mesh_indices = np.array(m.cells[0].data, dtype=np.int32).flatten()
+        else:
+            import openmesh
+            m = openmesh.read_trimesh(filename)
+            mesh_points = np.array(m.points())
+            mesh_indices = np.array(m.face_vertex_indices(), dtype=np.int32).flatten()
+        return wp.sim.Mesh(mesh_points, mesh_indices)
 
     def create_articulation(self, builder):
         wp.sim.parse_urdf(
@@ -60,11 +75,12 @@ class AllegroEnvironment(Environment):
                 os.path.dirname(__file__),
                 "../assets/isaacgymenvs/kuka_allegro_description/allegro.urdf"),
             builder,
-            xform=wp.transform(np.array((0.0, 0.3, 0.0)), wp.quat_rpy(-np.pi / 2, np.pi * 0.75, np.pi / 2)),
+            xform=wp.transform(np.array((0.0, 0.3, 0.0)) * self.scale, wp.quat_rpy(-np.pi / 2, np.pi * 0.75, np.pi / 2)),
             floating=False,
             base_joint="rx, ry, rz",
             density=1e3,
-            armature=0.01,
+            armature=0.001,
+            scale=self.scale,
             stiffness=1000.0,
             damping=0.0,
             shape_ke=1.e+3,
@@ -92,18 +108,27 @@ class AllegroEnvironment(Environment):
         cube_urdf_filename = os.path.join(
             os.path.dirname(__file__),
             "../assets/isaacgymenvs/objects/cube_multicolor_allegro.urdf")
-        cube_positions = [
+        cube_positions = np.array([
             (-0.1, 0.5, 0.0),
             (0.0, 0.05, 0.05),
             (0.01, 0.15, 0.03),
             (0.01, 0.05, 0.13),
-        ]
+        ]) * self.scale
+        object_shape = self.load_mesh(
+            os.path.join(os.path.dirname(__file__),
+                         "../assets/icosphere.obj"))
+        scale = 4e-2 * self.scale
         for pos in cube_positions:
+            # b = builder.add_body()
+            # builder.add_shape_mesh(mesh=object_shape, body=b, density=1e3, scale=(scale, scale, scale))
+            # builder.add_joint_free(b)
+            # builder.joint_q[-7:-4] = pos
             wp.sim.parse_urdf(
                 cube_urdf_filename,
                 builder,
                 xform=wp.transform(pos, wp.quat_identity()),
                 floating=True,
+                scale=self.scale,
                 density=1e2,
                 armature=0.0,
                 stiffness=0.0,
@@ -115,6 +140,9 @@ class AllegroEnvironment(Environment):
                 limit_ke=1.e+4,
                 limit_kd=1.e+1,
                 parse_visuals_as_colliders=False)
+            # for mesh in builder.shape_geo_src[-2:]:
+            #     if isinstance(mesh, wp.sim.Mesh):
+            #         mesh.remesh(visualize=True, )
 
         # builder.plot_articulation()
         builder.collapse_fixed_joints()
