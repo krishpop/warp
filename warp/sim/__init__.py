3fff2b2e49d9ea0e7f66715f1e6ab215659888e0
Krishnan <krshna@stanford.edu>
2023-11-08 17:06:08 -0500

    adds parse_partnet_urdf to wp.sim
    - fixes wildmeshing tetrahedralizer get_tet_mesh return unpacking

--- warp/sim/__init__.py
+++ warp/sim/__init__.py
@@ -46,6 +46,7 @@ from .articulation import eval_fk, eval_ik
 
 from .import_mjcf import parse_mjcf
 from .import_urdf import parse_urdf
+from .import_partnet import parse_partnet_urdf
 from .import_snu import parse_snu
 from .import_usd import parse_usd
 
